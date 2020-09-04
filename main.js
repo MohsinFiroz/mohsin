e1d6c323494787958170b26584307d9c74cbfbc3
Mohsin Firoz <m7firoz@gmail.com>
2020-09-04 19:17:45 +1000

    api test

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
-fetch('http://api.openweathermap.org/data/2.5/weather?q=Sydney&appid=d224fbf524bf9e668484a0bbef35cd5f')
+fetch('http://api.openweathermap.org/data/2.5/weather?q=Sydney&units=metric&appid=d224fbf524bf9e668484a0bbef35cd5f')
   .then(response => response.json())
-  .then(data => console.log(data));
+  .then(data => print(data.weather[0].main));
+
+  
+
+
+  function print(line) {
+    const appDiv = document.getElementById('home');
+    const div = document.createElement('div');
+    div.innerHTML = line;
+    appDiv.appendChild(div)
+  }
